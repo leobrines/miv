9ac0af6569e7c1fced660b6051b49d2f1a32def1
Leopoldo Brines <leobrines@gmail.com>
2016-07-24 07:31:21 -0400

    Add boot state

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO);
+
+game.state.add('boot', boot);
+
+game.state.start('boot');
---
+++ js/state/boot.js
@@ -0,0 +1,12 @@
+var boot = {
+	create: function (){
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+
+		game.stage.backgroundColor = '#212121';
+		game.world.enableBody = true;
+
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+	}
+};

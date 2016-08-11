8d0c3d198e81c5571f3eb3db122296f68bf5af17
Leopoldo Brines <leobrines@gmail.com>
2016-08-10 21:58:08 -0400

    Use dynamic port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var
 	server = require('http').Server(app);
 	// socket = require('socket.io')(server);
 
+var port = process.env.PORT || 5000;
+
 app.use('/', express.static('./'));
 app.use('/', express.static('./views'));
 
@@ -11,6 +13,6 @@ app.get('/', function (req, res){
 	res.sendFile('views/index.html');
 });
 
-server.listen(9080, function (){
-	console.log('Holaaaaaa. Server running on port 8080');
+server.listen(port, function (){
+	console.log('Server running on port ' + port);
 });
